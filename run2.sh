494b931122fe51dd876396d78308ee76fabda393
yuziGuo <gyh_fish@126.com>
2023-04-15 07:17:44 +0000

    rm

--- run2.sh
+++ run2.sh
@@ -1,11 +1,16 @@
 
+# python tune_with_optuna_clenshaw.py --dataset geom-squirrel --n-layers 2 --kw clengnn2layers --gpu 1  --id-log 0410017803 1> optunalogKlayers/sq-clenshaw2.log 2> optunalogKlayers/sq-clenshaw2.err 
+# sleep 3
+python tune_with_optuna_clenshaw.py --dataset geom-squirrel --n-layers 4 --kw clengnn4layers --gpu 1  --id-log 0410017803 1> optunalogKlayers/sq-clenshaw4.log 2> optunalogKlayers/sq-clenshaw4.err 
+sleep 3
+python tune_with_optuna_clenshaw.py --dataset geom-squirrel --n-layers 6 --kw clengnn6layers --gpu 1  --id-log 0410017803 1> optunalogKlayers/sq-clenshaw6.log 2> optunalogKlayers/sq-clenshaw6.err 
+sleep 3
 python tune_with_optuna_clenshaw.py --dataset geom-squirrel --n-layers 8 --kw clengnn8layers --gpu 1  --id-log 0410017803 1> optunalogKlayers/sq-clenshaw8.log 2> optunalogKlayers/sq-clenshaw8.err 
-sleep 3
-python tune_with_optuna_clenshaw.py --dataset geom-squirrel --n-layers 12 --kw clengnn12layers --gpu 1  --id-log 0410017803 1> optunalogKlayers/sq-clenshaw12.log 2> optunalogKlayers/sq-clenshaw12.err 
-sleep 3
-python tune_with_optuna_clenshaw.py --dataset geom-squirrel --n-layers 16 --kw clengnn16layers --gpu 1  --id-log 0410017803 1> optunalogKlayers/sq-clenshaw16.log 2> optunalogKlayers/sq-clenshaw16.err 
-sleep 3
-python tune_with_optuna_clenshaw.py --dataset geom-squirrel --n-layers 20 --kw clengnn20layers --gpu 1  --id-log 0410017803 1> optunalogKlayers/sq-clenshaw20.log 2> optunalogKlayers/sq-clenshaw20.err 
-sleep 3
-python tune_with_optuna_clenshaw.py --dataset geom-squirrel --n-layers 24 --kw clengnn24layers --gpu 1  --id-log 0410017803 1> optunalogKlayers/sq-clenshaw24.log 2> optunalogKlayers/sq-clenshaw24.err 
-sleep 3
+
+# python tune_with_optuna_clenshaw.py --dataset pubmedfull --n-layers 2 --kw clengnn2layers --gpu 0  --id-log 0410017803 1> optunalogKlayers/pub-clenshaw2.log 2> optunalogKlayers/pub-clenshaw2.err 
+# sleep 3
+# python tune_with_optuna_clenshaw.py --dataset pubmedfull --n-layers 4 --kw clengnn4layers --gpu 0  --id-log 0410017803 1> optunalogKlayers/pub-clenshaw4.log 2> optunalogKlayers/pub-clenshaw4.err 
+# sleep 3
+# python tune_with_optuna_clenshaw.py --dataset pubmedfull --n-layers 6 --kw clengnn6layers --gpu 0  --id-log 0410017803 1> optunalogKlayers/pub-clenshaw6.log 2> optunalogKlayers/pub-clenshaw6.err 
+# sleep 3
+# python tune_with_optuna_clenshaw.py --dataset pubmedfull --n-layers 8 --kw clengnn8layers --gpu 0  --id-log 0410017803 1> optunalogKlayers/pub-clenshaw8.log 2> optunalogKlayers/pub-clenshaw8.err 
